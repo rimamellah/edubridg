6c45b38680c23b7ad58b3c86c8055e25805cff78
Rima Mellah <rimamellah1324@gmail.com>
2025-06-17 03:28:01 +0300

    Updates

--- src/componets/SectionHeader.jsx
+++
@@ -1,9 +0,0 @@
-export default function SectionHeader({title, decription}){
-    return(
-      <div className="section-header">
-        <span className="text-primary">Practic Advice</span>
-        <h2>{title}</h2>
-        <p>{decription}</p>
-      </div>
-    )
-}
